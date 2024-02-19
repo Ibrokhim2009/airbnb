f539be3a1401c80a82c34557e9a8fba90790eeac
Ibrokhim2009 <ibrokhimakbarob@gmail.com>
2024-02-19 18:38:15 +0500

    feat-check

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './parts/navbar/Navbar'
-
+import { createContext } from 'react';
+export const Context = createContext()
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false);
   return (
-    <div> 
-      <Navbar/>
-    </div>
+    <Context.Provider value={{ isScrolled, setIsScrolled }}>
+      <Navbar />
+    <h1>lorem</h1>
+    </Context.Provider>
   )
 }
 
---
+++ src/pages/Admin.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Admin() {
+    return (
+        <form>
+            <input type="text" placeholder='img' name="img" id="" />
+            <input type="text" placeholder='name' name="name" id="" />
+            <input type="text" placeholder='location' name="location" id="" />
+            <input type="number" placeholder='distance' name="distance" id="" />
+            <input type="number" name="fistDay" placeholder='fistDay' id="" />
+            <input type="number" placeholder='secondDay' name="secondDay" id="" />
+            <input type="text" placeholder='month' name="month" id="" />
+            <input type="number" placeholder='price' name="price" id="" />
+            <input type="number" placeholder='reiting' name="reiting" id="" />
+            <input type="number" placeholder='reviews' name="reviews" id="" />
+        </form>
+    )
+}
+
+export default Admin
